a76ea9495526fd75b0bc5746966d0439d4248fb1
klin <45842843+imklin@users.noreply.github.com>
2026-08-20 17:02:06 +0800

    Add files via upload

--- keke_wubi_update_Windows.bat
+++ keke_wubi_update_Windows.bat
@@ -10,7 +10,7 @@ echo.
 
 :: 路径配置：全部临时文件移至系统缓存，不在Rime文件夹内
 set "RIMENEW=%APPDATA%\Rime"
-set "ZIP_TEMP=%temp%\KeKeWubi.zip"
+set "ZIP_TEMP=%temp%\Wubi.zip"
 set "TMP_DIR=%temp%\tmp_keke"
 set "UA=Chrome/120.0.0.0 Windows/10.0"
 
@@ -32,8 +32,10 @@ echo.
 :: ==================================================================
 
 :: 下载源：主用GitHub，可可五笔备用官网（文件内容完全一致）
-set "ZIP_GITHUB=https://github.com/KeKeWubi/Rime-KeKeWubi/archive/refs/heads/main.zip"
-set "ZIP_OFFICIAL=https://keke.kim/DownLoad/Rime-KeKeWubi-main.zip"
+set "ZIP_GITHUB=https://github.com/imklin/Rime-Wubi/archive/refs/heads/main.zip"
+
+set "ZIP_OFFICIAL=＊"
+
 
 :: 创建Rime文件夹（不存在则新建）
 if not exist "%RIMENEW%" md "%RIMENEW%"
@@ -90,7 +92,7 @@ echo.
 
 echo 4. 在系统缓存目录解压压缩包
 powershell Expand-Archive -Path "%ZIP_TEMP%" -DestinationPath "%TMP_DIR%" -Force
-if not exist "%TMP_DIR%\Rime-KeKeWubi-main" (
+if not exist "%TMP_DIR%\Rime-Wubi-main" (
     echo 错误：压缩包损坏，解压失败
     del "%ZIP_TEMP%"
     rmdir /s /q "%TMP_DIR%" 2>nul
@@ -99,7 +101,7 @@ if not exist "%TMP_DIR%\Rime-KeKeWubi-main" (
 )
 
 echo 5. 将仓库内所有文件直接复制到Rime根目录
-xcopy "%TMP_DIR%\Rime-KeKeWubi-main\*" "%RIMENEW%\" /e /h /y
+xcopy "%TMP_DIR%\Rime-Wubi-main\*" "%RIMENEW%\" /e /h /y
 
 echo 6. 清理系统全部临时文件
 rmdir /s /q "%TMP_DIR%"
@@ -119,4 +121,6 @@ echo ==============================================
 echo.
 pause
 start "" "%RIMENEW%"
-exit
+#https://github.com/KeKeWubi/Rime-KeKeWubi/archive/refs/heads/main.zip
+#https://keke.kim/DownLoad/Rime-KeKeWubi-main.zip
+exitexit
